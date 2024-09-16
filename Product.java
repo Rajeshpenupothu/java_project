c115b2f9a1e2df1cfad04f7c381b68d47276642e
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-09-17 01:19:11 +0530

    product

---
+++ Product.java
@@ -0,0 +1,50 @@
+public class Product{
+    private String name;
+    private String type;
+    private String place;
+    private int warranty;
+    
+    public Product(){
+
+    }
+
+    public Product(String name,String type,String place,int warranty){
+        this.name=name;
+        this.type=type;
+        this.place=place;
+        this.warranty=warranty;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name=name;
+    }
+    public String getType(){
+        return type;
+    }
+    public void setType(String type ){
+        this.type=type ;
+    }
+    public String getplace(){
+        return place;
+    }
+    public void setplace(String place){
+        this.place=place;
+    }
+    public int getwarranty(){
+        return warranty;
+    }
+    public void setwarranty(int warranty){
+        this.warranty=warranty;
+    }
+    @Override
+    public string tostring(){
+        return "Product{" +
+        "name='"+ name +'\''+
+        ",type='"+ type +'\''+
+        ",place='"+ place +'\''+
+        ",warranty="+warranty+'
+        }';
+    }
+    }
---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String[] args) {
+        
+        Product p=new Product();
+        System.out.println(p);
+    }
+}
